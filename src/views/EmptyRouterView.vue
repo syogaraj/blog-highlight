0938da6a9d445b72b496b25221eb5a5791d4fde0
Yogaraj <yoga30696@gmail.com>
2020-09-11 19:51:24 +0530

    added empty router views to support route children

---
+++ src/views/EmptyRouterView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-view name="create"></router-view>
+    <router-view name="oneBlog"></router-view>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptyRouterView"
+};
+</script>
+
+<style></style>
